9063da1dcf95500dcaea520e95af3fdf16a76979
Taj K <tkirk18@hotmail.com>
2019-12-08 00:32:57 -0600

    added Swappa OnePlus7 Pro tracking

--- priceSpider/priceSpider/spiders/discount.py
+++ priceSpider/priceSpider/spiders/discount.py
@@ -4,7 +4,29 @@ import scrapy
 
 class DiscountSpider(scrapy.Spider):
     name = 'discount'
-    start_urls = ['https://https://swappa.com/mobile/buy/oneplus-7-pro/unlocked/']
+    start_urls = ['https://swappa.com/mobile/buy/oneplus-7-pro/unlocked']
 
     def parse(self, response):
-        pass
+        count = 1
+        swappa_domain = "https://swappa.com"
+
+        try:
+            for row in response.xpath('//*[@id="listing_previews"]/div'):
+                description = row.xpath('.//a/@title').get()
+                link = swappa_domain + str(row.xpath('.//a/@href').get())
+
+                for tree in row.xpath('.//a/div/div'):
+                    for branch in tree.xpath('.//div/div/div'):
+                        price = branch.xpath('.//span/text()').get()
+                # print(f"#{count} ($" + str(price) + ") " + str(description) + " " + str(link))
+
+                yield {
+                    "description": description,
+                    "link": link,
+                    "price": price
+                }
+
+                count += 1
+
+        except Exception as e:
+            pass
