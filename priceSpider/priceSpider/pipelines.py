d61721d7a6812881fe3583e812d60cd4e510030f
Taj K <tkirk18@hotmail.com>
2019-12-11 20:00:52 -0600

    Inserting into Xbox Tuple

--- priceSpider/priceSpider/pipelines.py
+++ priceSpider/priceSpider/pipelines.py
@@ -24,17 +24,25 @@ class SwappaPipeline(object):
         global products_arr
         global existing_products
 
-        item_name = item["item"]
+        name = item["item"]
+        price = item["price"]
 
         if len(products_arr) == 0:
-            products_arr.append((f"{item_name}", []))
-            existing_products.append(item_name)
+            products_arr.append((f"{name}", []))
+            existing_products.append(name)
 
         for _ in products_arr:
-            if item_name not in existing_products:
-                existing_products.append(item_name)
-                products_arr.append((f"{item_name}", []))
+            if name not in existing_products:
+                existing_products.append(name)
+                products_arr.append((f"{name}", []))
                 break
+
+        if name == "Xbox One X":
+            if price <= 300:
+                for item_tuple in products_arr:
+                    if item_tuple[0] == name:
+                        item_tuple[1].append(item)
+
 
         # if item_name not in products_arr:
         #     print("Wasn't in there")
